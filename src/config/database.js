7e5f7842ec25323edeedf3afd76135b698b28d0d
zhengyuekai <zhengyuekai@hh-medic.com>
2024-07-30 12:44:14 +0800

    修改时区

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,20 @@
 module.exports = {
-  development: {
-    username: 'root',
-    password: 'yue123!@KAI',
-    database: 'node',
-    host: '8.130.106.155',
-    port: 3306,
-    dialect: 'mysql'
-  }
-  // development: {
-  //   username: 'root',
-  //   password: '123456',
-  //   database: 'node',
-  //   host: '127.0.0.1',
-  //   port: 4022,
-  //   dialect: 'mysql'
-  // }
+    development: {
+        username: 'root',
+        password: 'yue123!@KAI',
+        database: 'node',
+        host: '8.130.106.155',
+        port: 3306,
+        dialect: 'mysql',
+        timezone: 'Z'  // UTC time zone
+
+    }
+    // development: {
+    //   username: 'root',
+    //   password: '123456',
+    //   database: 'node',
+    //   host: '127.0.0.1',
+    //   port: 4022,
+    //   dialect: 'mysql'
+    // }
 };
